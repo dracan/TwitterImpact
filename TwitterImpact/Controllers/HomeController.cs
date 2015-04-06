b5b4952af75eb986d46b642010b9049d3c96e7af
Dan Clarke <dan@dracan.co.uk>
2015-04-06 10:27:00 +0100

    Added OAuthController (pinched from the Linq2Twitter demo files)

--- TwitterImpact/Controllers/HomeController.cs
+++ TwitterImpact/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LinqToTwitter;
 
 namespace TwitterImpact.Controllers
 {
@@ -10,6 +11,11 @@ namespace TwitterImpact.Controllers
     {
         public ActionResult Index()
         {
+            if(!new SessionStateCredentialStore().HasAllCredentials())
+            {
+                return RedirectToAction("Index", "OAuth");
+            }
+
             return View();
         }
 
